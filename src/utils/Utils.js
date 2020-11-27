de99cdcbe1bdd477b137acf52b8d11bfadbcc23c
Pedro Paulo <pedropaulotj@gmail.com>
2020-11-27 12:15:28 -0300

    ajustes

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,7 +1,7 @@
 const urlTheMealDB = "https://www.themealdb.com/api/json/v1/1";
 
-const apiLoginURL = "http://localhost:3010/user/login";
+const apiLoginURL = "https://desafio-onesight-api.herokuapp.com/user/login";
 
-const apiSignupURL = "http://localhost:3010/user/register";
+const apiSignupURL = "https://desafio-onesight-api.herokuapp.com/user/register";
 
 export default { urlTheMealDB, apiLoginURL, apiSignupURL };
