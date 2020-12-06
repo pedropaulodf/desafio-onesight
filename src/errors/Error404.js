a0d107d3cdd989a874af68d8616062e12d5e7a74
Pedro Paulo <pedropaulotj@gmail.com>
2020-12-06 19:48:17 -0300

    Estilo da página de erro 404

--- src/errors/Error404.js
+++ src/errors/Error404.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import css from './Error404.module.css';
 
 export default function Error404() {
   return (
-    <div>
-      <h1>404 Error!</h1>
-      <h2>Página não encontrada!</h2>
-      <Link to="/">Voltar par a home</Link>
+    <div className={css.container}>
+      <h1>Error 404!</h1>
+      <h2>Page not found!</h2>
+      <Link to="/">Click to go back</Link>
     </div>
-  )
+  );
 }
